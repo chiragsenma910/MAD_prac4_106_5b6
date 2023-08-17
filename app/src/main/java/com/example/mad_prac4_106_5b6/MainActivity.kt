eb3f31d7e929193c9196dab32b69beb837919c33
student <student@gnu.ac.in>
2023-08-17 12:35:11 +0530

    17/08/2023

---
+++ app/src/main/java/com/example/mad_prac4_106_5b6/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mad_prac4_106_5b6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.loginactivity_main)
+    }
+}
---
+++ app/src/main/java/com/example/mad_prac4_106_5b6/RegistrationMainActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.mad_prac4_106_5b6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrationMainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.registrationactivity_main2)
+    }
+}
